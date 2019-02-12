662e28c8d676302f45e43df9e01137e4e5391531
Brett Davidson <brett_davidson@yahoo.com>
2019-02-12 09:39:40 -0500

    Single Page Applications Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,11 +16,14 @@ import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
+
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+
 
 @NgModule({
   declarations: [
@@ -46,7 +49,8 @@ import { ContactComponent } from './contact/contact.component';
 
   ],
   providers: [
-    DishService
+    DishService,
+    PromotionService
   ],
   bootstrap: [AppComponent]
 })
